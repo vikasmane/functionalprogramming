80dac11efdf9285a2456244fd7f1cd1136fa39b9
vikasmane <vickymane@gmail.com>
2018-01-02 21:43:52 +0530

    Update bindAndApplyTwists.js

--- bindAndApplyTwists.js
+++ bindAndApplyTwists.js
@@ -2,4 +2,4 @@ var a = {a: 1}
 var myfunc = function (){return this.a + ' ' + arguments[0]};
 // var bindedMyfunc = Function.prototype.bind.apply(myfunc, [a, "param1", "param2"]);
 var bindedMyfunc = Function.prototype.apply.bind(myfunc, a, ['param1'])
-console.log(bindedMyfunc());
+console.log(bindedMyfunc()); //1 param1
